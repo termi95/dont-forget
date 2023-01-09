6662d4f062b2a292f8b1ac0379e3963f59665a78
DESKTOP-6Q4VF0O\Termi <sokolowski.a95@gmail.com>
2023-01-09 09:30:34 +0100

    add front with 2 views

---
+++ Frontend/src/views/RegisterView.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <form id="form">
+      <h1 class="center">Register</h1>
+      <label for="email">Email</label>
+      <input id="email" type="text" placeholder="user@email.com" />
+      <label for="password">Password</label>
+      <input id="password" type="password" placeholder="********" />
+      <div class="right">
+        <button class="space" type="submit">register</button>
+        <button>
+          <router-link to="/"> Login </router-link>
+        </button>
+      </div>
+    </form>
+  </main>
+</template>
+
+<style>
+@import url(../assets/login.style.css);
+</style>
+
+<script setup lang="ts"></script>
